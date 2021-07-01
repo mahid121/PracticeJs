51f92630334493bfada7a9619212608d2df21633
mrmahid06 <mrmahid06@gmail.com>
2021-07-01 12:31:41 +0100

    init existing files

---
+++ practice.js
@@ -0,0 +1,56 @@
+/*
+function nextInline(arr,item){
+    arr.push(item);
+    return arr.shift();
+}
+var testArray = [1,2,3,4,5];
+console.log(JSON.stringify(testArray));
+console.log(nextInline(testArray,6));
+console.log(JSON.stringify(testArray));
+
+function testStrict(val){
+    if(val === 7){
+        return "equal";
+    }
+    else
+     return "not equal";
+}
+console.log(testStrict(7));
+
+function isLess(a,b){
+    return a<b;
+}
+console.log(isLess(10,15));
+
+*/
+var count = 0;
+
+function cc(card)
+{
+    switch(card){
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 :
+            count++;
+            break;
+        case 10 :
+        case "J" :
+        case "Q" :
+        case "K" :
+        case "A" :
+            count--;
+            break;    
+        }
+        var holdBet = "Hold";
+        if(count > 0)
+        {
+            holdBet = "Bet";
+        }
+        return count + " "+holdBet;
+}
+console.log(cc("A"));
+console.log(cc("K"));
+console.log(cc("Q"));
+console.log(cc("J"));
---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+var age = 24;
+var myStr = "i am mahidur rahman and my age is "+age+"\n";
+console.log(myStr);
+var length=myStr.length;
+console.log(length);
+var first= myStr[0];
+console.log(first);
+var last = myStr[myStr.length-2];
+console.log(last); 
+
+function wordBlank(noun,adjective,verb,adverb){
+    result="";
+    result +="I am "+noun+" and i am a "+adjective+" boy and i "+verb+" nothing "+adverb+".";
+    return result;
+}
+console.log(wordBlank("mahid","bad","do","fast"));
+
+
+var myArray = [[1,2,3],[4,5,6],[7,8,9]];
+var myData = myArray[2][1];
+console.log(myData);
+var a = 2;
+console.log(typeof a);
+
+var outWear = "T-Shirt";
+
+function outFit(){
+    var outWear = "Sweater";
+    return outWear;
+}
+console.log(outFit());
+console.log(outWear);
+*/
+fetch("./config.json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
